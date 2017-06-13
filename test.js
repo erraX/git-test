4a63a44a739fbdcfdf54385345f66d10898c118e
niminjie <niminjie@baidu.com>
2017-06-13 16:30:08 +0800

    Revert "add bug"
    This reverts commit e871a1df528c515c8b9deeb919254c277b4ab842.

--- test.js
+++ test.js
@@ -5,7 +5,6 @@ define(
 
         console.log('hello world4');   
         console.log('hello world5');   
-        this is a bug
         console.log('bug blow');   
         console.log('hello world6');   
         console.log('hello world7');   
