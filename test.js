e7e31c660ab028d97095303b4ecf536836749f19
niminjie <niminjie@baidu.com>
2017-06-13 16:23:33 +0800

    add log

--- test.js
+++ test.js
@@ -6,6 +6,7 @@ define(
         console.log('hello world4');   
         console.log('hello world5');   
         this is a bug
+        console.log('bug blow');   
         console.log('hello world6');   
         console.log('hello world7');   
     }
