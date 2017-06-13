bcc2314c322523c27b326c8c7f18fddd4d54e05e
niminjie <niminjie@baidu.com>
2017-06-13 14:29:02 +0800

    Add test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+define(
+    function (require, exports, module) {
+        console.log('hello world1');   
+        console.log('hello world2');   
+        console.log('hello world3');   
+    }
+);
