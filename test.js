3b4a6943b495c2edff6e60e5b2e9b6adce724a3c
niminjie <niminjie@baidu.com>
2017-06-13 14:48:16 +0800

    test2

--- test.js
+++ test.js
@@ -2,5 +2,10 @@ define(
     function (require, exports, module) {
         console.log('hello world1');   
         console.log('hello world3');   
+
+        console.log('hello world4');   
+        console.log('hello world5');   
+        console.log('hello world6');   
+        console.log('hello world7');   
     }
 );
