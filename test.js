a7e702fdd113b10051f52b6e5f370517b1408b4c
niminjie <niminjie@baidu.com>
2017-06-14 12:02:30 +0800

    new feature 1

--- test.js
+++ test.js
@@ -8,5 +8,11 @@ define(
         console.log('bug blow22');   
         console.log('hello world6');   
         console.log('hellosdifasodfisadof world77777');   
+        console.log('bug blow');   
+        console.log('new feature2');
+        console.log('hello world6');   
+        console.log('hello world7');   
+
+        console.log('new feature1');
     }
 );
