e9890b8afe7841b589c8bb729ea8534001488255
niminjie <niminjie@baidu.com>
2017-06-14 18:21:02 +0800

    update mirror2

--- test.js
+++ test.js
@@ -7,6 +7,6 @@ define(
         console.log('hello world5');   
         console.log('bug blow22');   
         console.log('hello world6');   
-        console.log('hello world77777');   
+        console.log('hello world7777788888');   
     }
 );
