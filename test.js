719414bd3a5dce7628f14bfacb8488ee80e859d1
niminjie <niminjie@baidu.com>
2017-06-15 16:04:48 +0800

    Reset all code

--- test.js
+++ test.js
@@ -1,19 +1,4 @@
 define(
     function (require, exports, module) {
-        console.log('hello world1');   
-        console.log('hello world3');   
-
-        console.log('hello world4');   
-        console.log('hello world5');   
-        console.log('bug blow22');   
-        console.log('hello world6');   
-        console.log('hellosdifasodfisadof world77777');   
-        console.log('bug blow');   
-        console.log('new feature2');
-        console.log('hello world6');   
-        console.log('hello world7');   
-
-        console.log('new feature1');
-        console.log('new feature3');
     }
 );
